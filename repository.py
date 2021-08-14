f6d0b32d01d4fbc5144d4ea1c4d4eb7358e4585b
lee sang hun <oper6909@gmail.com>
2021-08-14 23:44:32 +0900

    add fake repository

--- repository.py
+++ repository.py
@@ -21,6 +21,19 @@ class AbstractRepository(abc.ABC):
     def get(self, reference) -> model.Batch:
         raise NotImplementedError
 
+class FakeRepository(AbstractRepository):
+
+    def __init__(self, batches):
+        self._batches = set(batches)
+    
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+    
+    def list(self):
+        return list(self._batches)
 
 class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session):
